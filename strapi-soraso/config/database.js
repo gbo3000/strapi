74dd2b83d3f7da369e10ba3c56cd06de781eb68e
Thanapol_r <thanapol@smartfinder.asia>
2024-01-19 18:03:33 +0700

    Configured production/database.json

--- strapi-soraso/config/database.js
+++ strapi-soraso/config/database.js
@@ -1,14 +1,32 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "localhost"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "sorasodb"),
+//       user: env("DATABASE_USERNAME", "postgres"),
+//       password: env("DATABASE_PASSWORD", "0000"),
+//       schema: env("DATABASE_SCHEMA", "public"), // Not required
+//       ssl: false
+//     },
+//     debug: false,
+//   },
+// });
+
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "sorasodb"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "0000"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: false
+      user: env("DATABASE_USERNAME", "thanapol"),
+      password: env("DATABASE_PASSWORD", "Shadow123"),
+      ssl: env.bool("DATABASE_SSL", false) && {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
     debug: false,
   },
